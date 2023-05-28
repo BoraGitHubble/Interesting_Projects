0ace977d9ee15b3a56040045d7cd22c78d2c1e44
BORA <134833285+BoraGitHubble@users.noreply.github.com>
2023-05-28 13:12:57 +0200

    Delete Grammar_Check.py

--- Grammar_Check.py
+++
@@ -1,6 +0,0 @@
-pip install gingerit
-# This code will check your English sentence for grammar mistakes
-from gingerit.gingerit import GingerIt
-text = input("Enter a sentence >>: ")
-corrected_text = GingerIt().parse(text)
-print(corrected_text['result'])
